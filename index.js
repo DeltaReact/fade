8fa1a98412eb858bd171e7610baa28f2a07569ce
Delta React User <mail@deltareact.com>
2016-04-24 21:19:02 +0100

    jQuery upgrade work.

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ var fade = {};
 module.exports = fade;
 
 fade.fade = function fade (element, isIn, callback, inValue, outValue) {
-  if (inValue === undefined) var inValue = 1;
-  if (outValue === undefined) var outValue = 0;
+  if (inValue === undefined) inValue = 1;
+  if (outValue === undefined) outValue = 0;
   var fadeTime = 1000;
   var $element = $(element);
   if (isIn === true) {
-    element.style.display = (element.hasAttribute("data-fade-inline-block") ? "inline-block" : "block");
-    $element.stop().animate({opacity: inValue}, fadeTime, "swing", callback);
+    $element.css("display", ($element.attr("data-fade-inline-block") ? "inline-block" : "block"));
+    $element.stop().animate({ "opacity": inValue }, fadeTime, "swing", callback);
   } else {
-    $element.stop().animate({opacity: outValue}, fadeTime, "swing", function () {
-      if (outValue === 0) element.style.display = "none";
+    $element.stop().animate({ "opacity": outValue }, fadeTime, "swing", function () {
+      if (outValue === 0) $element.css("display", "none");
       return callback();
     });
   }
