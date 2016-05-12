d9cf3831b88f012d5cde20daf960883c13e6a88b
Delta React User <mail@deltareact.com>
2016-05-12 10:49:39 +0100

    Some more work to remove jQuery.

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ fade.fade = function fade (element, isIn, callback, inValue, outValue) {
   if (inValue === undefined) inValue = 1;
   if (outValue === undefined) outValue = 0;
   var fadeTime = 1000;
-  var $element = $(element);
   if (isIn === true) {
-    $element.css("display", ($element.attr("data-fade-inline-block") ? "inline-block" : "block"));
-    $element.stop().animate({ "opacity": inValue }, fadeTime, "swing", callback);
+    element.style.display = (element.dataset.fadeInlineBlock ? "inline-block" : "block");
+    $(element).stop().animate({ "opacity": inValue }, fadeTime, "swing", callback);
   } else {
-    $element.stop().animate({ "opacity": outValue }, fadeTime, "swing", function () {
-      if (outValue === 0) $element.css("display", "none");
+    $(element).stop().animate({ "opacity": outValue }, fadeTime, "swing", function () {
+      if (outValue === 0) element.style.display = "none";
       return callback();
     });
   }
