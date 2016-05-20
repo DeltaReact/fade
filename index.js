d8ae5cd42755755277a1de04b537e459c4edb910
Delta React User <mail@deltareact.com>
2016-05-20 03:55:52 -0700

    Fix.

--- index.js
+++ index.js
@@ -6,31 +6,36 @@ module.exports = fade;
 
 fade.fade = function fade (element, isIn, callback, inValue, outValue) {
 
-  var internalFade = function (element, value, fadeTime, callback) {
+  var internalFalseyCallback = function () {
+    return callback(false);
+  };
+
+  var internalFade = function (element, value, callback) {
+    Velocity(element, "stop");
     Velocity(element,
       {
         "opacity": value
       },
       {
-        "duration": fadeTime,
-        "complete": callback
+        "duration": 1000,
+        "complete": function () {
+          return internalFalseyCallback();
+        }
       }
     );
-    return false;
   };
 
   if (inValue === undefined) inValue = 1;
   if (outValue === undefined) outValue = 0;
   if (!callback) callback = (function () {  });
-  var fadeTime = 1000;
   if (element[0]) element = element[0];
   if (isIn === true) {
-    element.style.display = (element.dataset.fadeInlineBlock ? "inline-block" : "block");
-    return internalFade(element, inValue, fadeTime, callback);
+    element.style.display = (element.dataset.fadeInlineBlock === "" ? "inline-block" : "block");
+    return internalFade(element, inValue, callback);
   } else {
-    return internalFade(element, outValue, fadeTime, function () {
+    return internalFade(element, outValue, function () {
       if (outValue === 0) element.style.display = "none";
-      return callback();
+      return internalFalseyCallback();
     });
   }
 };
